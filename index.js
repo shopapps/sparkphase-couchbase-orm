1510e0fcac5d3c084405cf330c9daa3986087f49
JonathanBristow <jonathanbristow@jonathans-mbp.home>
2014-10-17 15:27:45 +0100

    Update

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 /*
-	Sparky Version 0.1.0
+	Sparkphase Couchbase ORM Version 0.1.0
 	Updated: Friday 17th October 2014
 	Author: Jonathan Bristow <jonathanbristow@me.com>
 	Repository: https://github.com/JonathanBristow/Sparky
 */
 
-module.exports = function Sparky(Options) {
-	console.log(Options.Type+' => '+Options.Group+' => '+Options.Message);	
-	if (Options.Detail) {
-		console.log('Detail:', Options.Detail);
-	}
-	if (Options.Exit) {
-		console.log('This error has caused the application to exit.');
-		process.exit();
-	}
+module.exports = function(Options) {
+
+
+
+
 }
